84c9a2c1e6f84470f97bb667b3ee1b6987a77d03
Mason Hall <masonhall@gmail.com>
2019-04-10 20:41:06 -0500

    adding custom orders

--- Arduino-slaves/arduino-board/order.h
+++ Arduino-slaves/arduino-board/order.h
@@ -2,14 +2,22 @@
 #define ORDER_H
 
 // Define the orders that can be sent and received
-enum Order {
-  HELLO = 0,
-  SERVO = 1,
-  MOTOR = 2,
-  ALREADY_CONNECTED = 3,
-  ERROR = 4,
-  RECEIVED = 5,
-  STOP = 6,
+enum Order
+{
+    HELLO = 0,
+    SERVO = 1,
+    MOTOR = 2,
+    ALREADY_CONNECTED = 3,
+    ERROR = 4,
+    RECEIVED = 5,
+    STOP = 6,
+    MOTOR1 = 11,
+    MOTOR2 = 12,
+    MOTOR3 = 13,
+    MOTOR4 = 14,
+    MOTOR5 = 15,
+    MOTOR6 = 16
+
 };
 
 typedef enum Order Order;
--- RaspberryPi-master/tester.py
+++ RaspberryPi-master/tester.py
@@ -4,7 +4,6 @@ import time
 import struct
 from Arduino import Arduino
 from robust_serial import write_order, Order, write_i8, write_i16, read_i8, read_order
-from robust_serial.utils import open_serial_port
 
 ard1 = Arduino(port="/dev/ttyACM0",speed=9600)
 
