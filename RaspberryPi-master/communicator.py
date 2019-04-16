edf54648d9d9b4b61892b856c7b1b16f38094c34
Mason Hall <masonhall@gmail.com>
2019-04-16 16:32:00 -0500

    testing handshake

--- RaspberryPi-master/communicator.py
+++ RaspberryPi-master/communicator.py
@@ -44,7 +44,7 @@ def setup():
 
 def stopAll(ardList):
     for ard in ardList:
-        for motor in range(6)
+        for motor in range(6):
             write_order(ard.conn, Order.MOTOR)
             write_i8(ard.conn, motor+1)
             write_i8(ard.conn, 0)
